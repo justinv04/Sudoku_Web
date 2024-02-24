f1901a5a952013e70aa7a2957bab6913f4af7bbe
justinv04 <njkvincent@gmail.com>
2024-02-23 18:31:34 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function changeLeaderboardTimeRange(newTimeRangeLabel)
+{
+    document.getElementById("Leaderboard_TimeRange").innerHTML = newTimeRangeLabel;
+}
+
+function changeLeaderboardDifficult(newDifficultyLabel)
+{
+    document.getElementByID("Leaderboard_Difficulty").innerHTML = newDifficultyLabel;
+}
